4814f93fdb6d45700d2b34cbcae94cc91b0c180a
DAUTREY <mikael.dautrey@isitix.com>
2016-05-19 14:36:44 +0200

    Initial commit

---
+++ src/main/java/springaop/SpringaopApplication.java
@@ -0,0 +1,12 @@
+package springaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaopApplication.class, args);
+	}
+}
